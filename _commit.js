7d922c18bcf2cf45d3e36abe547bc762cce24670
wmwlwmwl <2304153226@qq.com>
2026-06-22 00:43:37 +0800

    fix: delete _commit.js and update i18n/components

--- _commit.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-execSync('git add -A', { cwd: 'c:\\D\\Lumin-SSH', stdio: 'inherit' });
-execSync('git commit -m "删除fnOS和FygoOS图标"', { cwd: 'c:\\D\\Lumin-SSH', stdio: 'inherit' });
-execSync('git push', { cwd: 'c:\\D\\Lumin-SSH', stdio: 'inherit' });
